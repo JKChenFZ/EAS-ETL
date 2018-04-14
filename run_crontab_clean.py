f8aa02fc5c4a645143761a2f804a9ce77539863a
Kun Chen <KunFZ@gatech.edu>
2018-04-14 00:25:26 -0400

    more crontab

--- run_crontab_clean.py
+++ run_crontab_clean.py
@@ -1,5 +1,9 @@
+import datetime
 from load_history.crontab import EASCrontabUtil
 
 if __name__ == '__main__':
+    print('--------------Start-----------------------------')
+    print(datetime.datetime.utcnow())
     util = EASCrontabUtil()
     util.run_clean()
+    print('--------------Done------------------------------')
--- run_crontab_load.py
+++ run_crontab_load.py
@@ -1,5 +1,10 @@
 from load_history.crontab import EASCrontabUtil
+import datetime
 
 if __name__ == '__main__':
+    print('--------------Start-----------------------------')
+    print(datetime.datetime.utcnow())
     loader = EASCrontabUtil()
     loader.run_loader()
+    print('--------------Done------------------------------')
+
